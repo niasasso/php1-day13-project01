94564f11480ca8365925b543bcc427d0cf2bb41a
Sulenias Asso <74882893+niasasso@users.noreply.github.com>
2022-07-04 20:27:13 +0900

    final day 13

--- pengguna.php
+++ pengguna.php
@@ -1,3 +1,16 @@
+<?php 
+
+	// INTEGRASIS KONEKSI
+
+	require_once('connection.php');
+
+	// MENGAMBIL DATA "pengguna" DARI DATABASE
+	$sql = "SELECT * FROM pengguna";
+
+	$result = $connection->query($sql);
+
+ ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,7 +24,7 @@
 	<div class="container">
 		<div class="row my-5">
 			<!-- SIDEBAR -->
-			<div class="col-lg-4">
+			<div class="col-lg-4 p-0 px-lg-4">>
 				<div class="list-group">
 					<a href="index.php" class="list-group-item list-group-item-action">Dasbor</a>
 					<a href="pengguna.php" class="list-group-item list-group-item-action">Pengguna</a>
@@ -22,7 +35,83 @@
 
 			<!-- CONTENT -->
 			<div class="col-lg-8 bg-light rounded border shadow">
-				<h1>Pengguna</h1>
+				<div class="d-flex justify-content-between">
+					<div>
+						<h1>Pengguna</h1>
+					</div>
+					<div class="mt-3">
+						<a href="pengguna-tambah.php" class="btn btn-sm btn-primary">Tambah</a>
+					</div>
+				</div>
+				
+
+				<!-- .table -->
+
+				<table class="table table-hover">
+				  <thead>
+				    <tr>
+				      <th scope="col">#</th>
+				      <th scope="col">Nama Lengkap</th>
+				      <th scope="col">Tempat, Tanggal Lahir</th>
+				      <th scope="col">Opsi</th>
+				    </tr>
+				  </thead>
+				  <tbody>
+
+				  	<?php 
+				  	$no = 1;
+				  		if($result->num_rows > 0){
+
+				  			$jumlah_data = $result->num_rows;
+
+				  			// echo 'Jumlah Data : ' . $jumlah_data;
+
+				  			while($row = $result->fetch_assoc()){
+
+				  				echo "<tr>";
+
+				  				echo "<td>".$no."</td>";
+				 
+			  					echo "<td>" 
+			  					. $row['nama_depan'] 
+			  					. " " 
+			  					. $row['nama_tengah']
+			  					. " " 
+			  					. $row['nama_belakang'] 
+			  					. "</td>";
+
+			  					echo "<td>" 
+			  					. $row['tempat_lahir'] 
+			  					. " " 
+			  					. $row['tanggal_lahir']
+			  					. " " 
+			  					. $row['bulan_lahir']
+			  					. " "
+			  					. $row['tahun_lahir']
+			  					. "</td>";
+
+			  					echo "
+		  							<td>
+								      	<a href='pengguna-detail.php' class='btn btn-sm btn-success'>Detail</a>
+								      	<a href='pengguna-ubah.php' class='btn btn-sm btn-primary'>Ubah</a>
+								      	<a href='pengguna-hapus.php' class='btn btn-sm btn-danger'>Hapus</a>
+								      </td>";
+				  					
+				  				echo "</tr>";
+				  				$no++;
+
+				  			}
+
+				  		} else {
+				  			echo "Tidak ada data di database";				  				
+				  		}
+				  	 ?>
+				    
+				  </tbody>
+				</table>
+
+				<!-- end .table -->
+
 			</div>
 			<!-- END CONTENT -->
 		</div>
